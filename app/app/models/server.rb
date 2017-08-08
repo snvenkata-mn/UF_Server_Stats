2d72b29c56eb9b9f40bbca097cff5865d52518d5
root <root@mnipdsnvenkata.modeln.com>
2017-08-08 07:15:00 -0700

    Adding Changes

--- app/app/models/server.rb
+++ app/app/models/server.rb
@@ -1,2 +1,4 @@
 class Server < ApplicationRecord
+
+  belongs_to :project
 end
--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+    { en: 'Astellas'},
+    { en: 'Merck'},
+    { en: 'J&J'},
+    { en: 'Lupin'},
+    { en: 'Fresenius'},
+    { en: 'Leo'},
+    { en: 'Edwards'},
+].each do |name|
+  project = Project.find_or_initialize_by(name: name[:en])
+  project.update_attributes({
+                                name: name[:en]
+                            })
+end
